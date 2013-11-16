8bd64e8fb51b8980dfbe466c168d159c15234b71
wangyinfeng <wang.batman@gmail.com>
2013-11-16 21:27:40 +0800

    add template for this dir

---
+++ template.c
@@ -0,0 +1,26 @@
+/*====================================================================
+ * Description: 
+ * DATE: 2013/10/17
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <string.h>
+#include <stdio.h>  
+#include <time.h>  
+#include <sys/time.h>  
+#include <stdlib.h>  
+#include <signal.h>
+
+
+/* Macro definition */
+#define false           0
+#define true            1
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
