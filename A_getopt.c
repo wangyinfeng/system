4af42515ea2795a467431faf697147ac0dcaae97
wangyinfeng <wang.batman@gmail.com>
2014-03-12 15:06:40 +0800

    use getopt_long()

--- A_getopt.c
+++ A_getopt.c
@@ -8,8 +8,8 @@
        --help display this help and exit
  *}
  * DATE: 2013/12/26
- * Modify:
- * Conclusion:
+ * Modify: 2014/03/12 implement with getopt_long();
+ * Conclusion: life is short, save the time, use existed tools
 ===================================================================*/
 /* include */
 #include <stdio.h>
@@ -32,26 +32,35 @@ int getopt(int argc, char * const argv[],
            const char *optstring); //a string containing the legitimate option characters.
 */
 
-/*
-int getopt_long(int argc, char * const argv[], //accept long option, start with 2 dashes.
-           const char *optstring, // pointer to the 
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+const char* short_option = "m:vh";
+static struct option long_option[] = { 
+        {"module",  required_argument,  NULL, 'm'},
+        {"version", no_argument,        NULL, 'v'},
+        {"help",    no_argument,        NULL, 'h'},
+        {NULL,      0,                  NULL, 0} /* mandatory at the end */
+    };
+/* {full-name, have-argument? 1:0, ?, short-name} */
+
+/* accept long option, start with 2 dashes.
+- parse the options one by one, return the short option
+- return -1 if no more options
+- return ? if option not in scope
+
+int getopt_long(int argc, char * const argv[], 
+            const char *optstring, // pointer to the short options  
+            const struct option *longopts, 
+                               // pointer to the long options
                                struct option {
                                     const char *name;
                                     int         has_arg;
                                     int        *flag;
                                     int         val;
                                 };
-           const struct option *longopts, int *longindex);
+            int *longindex);
 */
-
-extern char *optarg;
-extern int optind, opterr, optopt;
-
-static struct option long_option[] = { 
-        {"mode",    required_argument,  0, 0},
-        {"verbose", no_argument,        0, 0},
-        {"help",    no_argument,        0, 0}
-    };
 
 int
 main(int argc, char **argv)
@@ -59,6 +68,12 @@ main(int argc, char **argv)
     int opt;
     char module[32];
     memset(module, 0x0, sizeof(module));
+    if (argc < 2)
+        printf("usage: \n" /* use "" to combine multiline is better than use '\' */
+        "    -m/--module MODULE: show module\n"
+        "    -v/--version: show verion\n"
+        "    -h/--help: show help info\n");
+#if USE_GETOPT
     while((opt = getopt(argc, argv, "m:vh")) != -1) { /* until to the end */
 //        printf("getopt %d\n", opt);
         switch(opt) {
@@ -72,6 +87,30 @@ main(int argc, char **argv)
             case 'h':
                 printf("help\n");
                 break;
+            case '?': /* parameter not in scope will return '?' */
+                printf("unknow parameter\n");
+                break;
+            default:
+                printf("unknow argument %c\n", (char)opt);
+                exit(EXIT_FAILURE);    
+        }
+    }
+#endif
+    while((opt = getopt_long(argc, argv, "m:vh", long_option, NULL)) != -1) { /* until to the end */
+        switch(opt) {
+            case 'm':
+                memcpy(module, optarg, strlen(optarg));
+                printf("module: %s\n", module);
+                break;
+            case 'v':
+                printf("version\n");
+                break;
+            case 'h':
+                printf("help\n");
+                break;
+            case '?': /* parameter not in scope will return '?' */
+                printf("unknow parameter: %c\n", (char)opt);
+                break;
             default:
                 printf("unknow argument %c\n", (char)opt);
                 exit(EXIT_FAILURE);    
@@ -79,4 +118,5 @@ main(int argc, char **argv)
     }
     return 0;
 }
+
 
