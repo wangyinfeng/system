6ec03dae396bb22070468891aa0e1bd60cfa161c
wangyinfeng <wang.batman@gmail.com>
2014-03-18 18:03:21 +0800

    TBD...

---
+++ A_condition_variable.c
@@ -0,0 +1,26 @@
+/*====================================================================
+ * Description: TBD...Implement the condition variable at page 114.
+ * know the condition variable.
+ * DATE: 2014/03/18
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+/* Macro definition */
+#define false           0
+#define true            1
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
