168fdf1103c44780d2fc0ef58585c62de8a6d3f6
wangyinfeng <wang.batman@gmail.com>
2014-03-18 11:22:30 +0800

    add cleanup handle

--- A_thread_specific_data.c
+++ A_thread_specific_data.c
@@ -49,11 +49,33 @@ void *thread_func(void *argv)
     return NULL;
 }
 
+void release(void *buff)
+{
+    if (buff != NULL)
+        free(buff);
+}
+
+void *cleanup_handle(void *argv)
+{
+    char *buff = malloc(100);
+    if (buff != NULL)
+        pthread_cleanup_push(release,buff);
+    /* do something here */
+    /* if the thread be terminated or exit with pthread_exit without
+     * relase the buffer, the cleanup_handle(release) will release the
+     * buffer. */
+
+    /* pass non-zero parameter to the unregister function, will actually
+     * run the cleanup function. the same as use free() explicitly. */
+    pthread_cleanup_pop(1);
+}
+
 int
 main(int argc, char *argv[])
 {
     int i;
     pthread_t thread[2];
+    pthread_t cleanup_thread;
 
     /* create the key, and also specify the destructor */
     pthread_key_create(&thread_log_key, close_log);
@@ -65,6 +87,8 @@ main(int argc, char *argv[])
         if (!pthread_equal(pthread_self(), thread[i]))
             pthread_join(thread[i], NULL);
     }
+
+    pthread_create(&cleanup_thread, NULL, cleanup_handle, NULL);
 
     fprintf(stdout, "End of main\n");
     return 0;
