1f38ee6717893c4998646c496b574b8c6fc3cc4c
Wang Yinfeng <wang.batman@gmail.com>
2015-02-12 01:08:57 -0500

    find out the double linked list usage for the task_struct

---
+++ LKD_iterate_all_progress.c
@@ -0,0 +1,30 @@
+/*====================================================================
+ * Description: Iterate all progress with the task_struct, begin with the current progress
+ * DATE: 2015/02/12
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* includes */
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+/* Macros */
+#define false           0
+#define true            1
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
